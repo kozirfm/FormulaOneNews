c4a9230af70c591792347f3f4107cde29469e1d1
Aleksandr Kazirickiy <kozirfm@yandex.ru>
2021-10-12 20:35:09 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.android")){
+                useModule("com.android.tools.build:gradle:7.0.2")
+            }
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")){
+                useVersion("1.5.30")
+            }
+        }
+    }
+}
+rootProject.name = "Formula One News"
+include(":app")
+include(":news")
+include(":network")
+include(":utils")
+include(":navigation")
+include(":core")
+include(":news-api")
+include(":design-system")
+include(":navigation-api")
+include(":navigation-graph")
+include(":network-api")
